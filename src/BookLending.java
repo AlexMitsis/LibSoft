b8f7a8895ac51e9a53257d74df6d374405ae19f4
AlexMitsis <62258971+AlexMitsis@users.noreply.github.com>
2020-05-14 02:17:19 +0300

    added template from SDD

--- src/BookLending.java
+++ src/BookLending.java
@@ -1,4 +1,19 @@
 
-public class BookLending {
-
+public class BookLending 
+{
+  private Borrower borrower;
+  private Book book;
+  private int renewalCounter;
+  private int timeLeft;
+  
+  public  int getTimeLeft()
+  {
+    
+  }
+    
+  public void RenewLendingPeriod(Borrower b)
+  {
+    
+  }
+  
 }
