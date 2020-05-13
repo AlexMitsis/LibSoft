a3b1f2d493344999a0b60f691f4cda18072fd445
AlexMitsis <62258971+AlexMitsis@users.noreply.github.com>
2020-05-14 02:15:04 +0300

    added the template from the SDD doc

--- src/Author.java
+++ src/Author.java
@@ -1,5 +1,24 @@
 
 public class Author {
+  
+  private String name;
+  private ArrayList<Book> ListOfBooks;
+  private ArrayList<Borrower> ListOfFollowers;
+  
+  public void deleteFollower(Borrower b)
+  {
+    
+  }
+  
+  public void addFollower(Borrower b)
+  {
+    
+  }
+  
+  public void updateListOfBooks()
+  {
+    
+  }
 
 }
 //ο αθορ εχει λιστα με τα βιβλια του και λιστα με τους ακολουθους του,νε την απντειτ που θα καλει ο βιβλιοθηκαριος
